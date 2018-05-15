a8be782fdc8f8af34c0b81eb7b8b0671ff280d07
mateUo <janowicz.mateusz@gmail.com>
2018-05-15 11:00:11 +0200

    Automated commit for Ansible powered by cron

--- snapper_config_recreation.sh
+++ snapper_config_recreation.sh
@@ -4,7 +4,6 @@ x=0
 volumes=$(mount | grep 'subvol=/)' | awk '{print $3}' | sed -r 's/[/]//;/^$/d')
 
 for volume in ${volumes}; do
-
 	shares=$(btrfs subv list /${volume} | sed -r '/.snapshots|home|\./d' | awk '{print $9}')
 
 	for share in ${shares}; do
--- snapshots_deletion_rnnml.sh
+++ snapshots_deletion_rnnml.sh
@@ -3,7 +3,6 @@
 volumes=$(mount | grep 'subvol=/)' | awk '{print $3}' | sed -r 's/[/]//;/^$/d')
 
 for volume in ${volumes}; do 
-	
 	shares=$(btrfs subv list /${volume} | sed -r '/.snapshots|home|\./d' | awk '{print $9}')
 	
 	for share in ${shares}; do
@@ -11,7 +10,6 @@ for volume in ${volumes}; do
 		for snap_number in $(rn_nml -Q snapshot:/${volume}/${share} | grep 'Snapshot_Name' | cut -d '>' -f2 | cut -d '<' -f1); do 
 
 			for type in {1..2}; do	
-
 				rn_nml -d snapshot:/${volume}/${share}@${snap_number}:${type}
 			       	sleep 10
 		       	done
