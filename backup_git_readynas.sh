7b7a7a94e8031cb9a6b6f202d1b5feef4220a57c
mateUo <janowicz.mateusz@gmail.com>
2018-05-15 12:00:13 +0200

    Automated commit for ReadyNAS powered by cron

--- backup_git_readynas.sh
+++ backup_git_readynas.sh
@@ -9,5 +9,5 @@ if [[ -n $to_remove ]]; then
     git rm --ignore-unmatch $to_remove 2>/dev/null 2>&1
 fi
 
-git commit -m "Automated commit for Ansible powered by cron"
+git commit -m "Automated commit for ReadyNAS powered by cron"
 git push -u origin master
