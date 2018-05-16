a1bd70dc37963934b78b8e7c71fe6d2dd61315a6
mateUo <janowicz.mateusz@gmail.com>
2018-05-16 16:14:19 +0200

    Automated commit for ReadyNAS powered by cron

--- smartctl_NAS.sh
+++ smartctl_NAS.sh
@@ -8,30 +8,30 @@ output2=$(cat /proc/partitions | grep -w sd[a-z] | awk '{print $4}' | tr '\n' ' 
 printf "\n\n##########Hello $(whoami), welcome to the smartctl utility!##########\n\n"
 
 #Main loop:
-while true
-do
-printf "\n\n"
+while true; do
+
+	printf "\n\n"
 	read -p "Please define the disk label (sda or sdb, etc.) or type exit to quit: " -n 4 -t 10 -e disk
 
-if [[ $disk == "EXIT" ]] || [[ $disk == "exit" ]]; then
-	printf "\n\nGoodbye!\n\n\n"
-break
+        if [[ $disk == "EXIT" ]] || [[ $disk == "exit" ]]; then
+		printf "\n\nGoodbye!\n\n\n"
+        break
 
-elif [[ -z $disk ]]; then
-continue
+        elif [[ -z $disk ]]; then
+        continue
 
-elif [[ $disk == "sd" ]] || [[ $disk == "SD" ]] || [[ $disk == "hd" ]] || [[ $disk == "HD" ]]; then
-        printf "\n\nSuch label doesn't exist here, try again\n"
-continue
+        elif [[ $disk == "sd" ]] || [[ $disk == "SD" ]] || [[ $disk == "hd" ]] || [[ $disk == "HD" ]]; then 
+		printf "\n\nSuch label doesn't exist here, try again\n"
+        continue
 
-elif [[ $output =~ $disk ]] && [[ $disk =~ ^$output2 ]]; then 
-	printf "\n\n\n==========/dev/$disk==========\n\n\n\n"
-        sudo smartctl --all /dev/$disk | egrep -i 'serial|model|sector|error|hour'
-continue   
+        elif [[ $output =~ $disk ]] && [[ $disk =~ ^$output2 ]]; then 
+		printf "\n\n\n==========/dev/$disk==========\n\n\n\n"
+                sudo smartctl --all /dev/$disk | egrep -i 'serial|model|sector|error|hour'
+        continue   
 
-else
-	printf "\n\nSuch label doesn't exist here, try again\n"
-continue
+        else
+		printf "\n\nSuch label doesn't exist here, try again\n"
+        continue
 
-fi
+        fi
 done
